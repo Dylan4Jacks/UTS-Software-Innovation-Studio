7a38e64e2a9ba05b1ab67687c52fec38d41a858e
Braden <bradenwilkes@outlook.com>
2023-09-05 12:29:55 +1000

    Card Generation Script
    - Added base card generation script
    - Spawns 10 cards with random stats

---
+++ Assets/Scripts/spawnPrefabs.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPrefabs : MonoBehaviour
+{
+    public GameObject cardPrefab;
+    public GameObject hand;
+
+    void Start()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        GameObject playerHand = Instantiate(hand, new Vector3(0, 0, 0), Quaternion.identity);
+        for (int i = 0; i < 10; i++)
+        {
+            generateCard(playerHand);
+        }
+    }
+
+    private void generateCard(GameObject hand)
+    {
+        GameObject createdCard = Instantiate(cardPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        createdCard.GetComponent<cardStats>().speed = Random.Range(1, 101);
+        createdCard.GetComponent<cardStats>().strength = Random.Range(1, 101);
+        createdCard.GetComponent<cardStats>().health = Random.Range(1, 101);
+        createdCard.transform.parent = hand.transform;
+    }
+}
