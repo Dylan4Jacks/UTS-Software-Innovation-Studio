c4f83bbdaffdfc2c38ed48dbce429810acb74266
oattions <oattions@gmail.com>
2023-09-07 11:58:14 +1000

    moved my scripts into the script folder, and fixed some bugs in file naming inconsistency

--- Assets/Scripts/TeamSlot.cs
+++ Assets/Scripts/TeamSlot.cs
@@ -2,24 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card {
-    public string name;
-    public int attack;
-    public int speed;
-    public int health;
-
-    public Card(string name, int attack, int speed, int health) {
-        this.attack = attack;
-        this.speed = speed;
-        this.health = health;
-    }
-}
 
 public class TeamSlot : MonoBehaviour
 {
-    private Card? baseCard;
+    #nullable enable
+    private Card? assignedCard;
+    #nullable disable
     public int currentHealth;
-    public int currentAttack;
+    public int currentStrength;
     public int currentSpeed;
     
     // Start is called before the first frame update
@@ -34,15 +24,15 @@ public class TeamSlot : MonoBehaviour
     }
     
     public bool hasCard() {
-        return (baseCard != null);
+        return (assignedCard != null);
     }
 
     public void takeDamage(int damage) {
         this.currentHealth -= damage;
     }
     public void dealDamage(TeamSlot defender) {
-        defender.takeDamage(this.currentAttack);
-        this.takeDamage(defender.currentAttack);
+        defender.takeDamage(this.currentStrength);
+        this.takeDamage(defender.currentStrength);
     }
 }
 
