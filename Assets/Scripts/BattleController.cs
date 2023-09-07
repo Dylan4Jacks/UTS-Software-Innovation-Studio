1eb26fe7e82cafe357759dfdc5cecb60b9f0fd82
oattions <oattions@gmail.com>
2023-09-07 12:35:25 +1000

    Refactored teams. Game objects populate under teams properly now

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -1,31 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Team {
-
-    //array positions 0 - 5 in order
-    // 0.front row left, 1.front row middle, 2.front row right
-    // 3.back row left, 4.back row middle, 5.back row right
-    public List<Card> teamPositions = new List<Card>() {null, null, null, null, null, null};
-
-    public Team(
-        
-        ) {
-            //all teams initialised empty
-        }
-
-    public void assignTeamSlot(int position, Card card) {
-        if (position <= 5 && position >= 0) {
-            teamPositions[position] = card;
-        }
-    }
-}
-
 public class BattleController : MonoBehaviour
 {
-    public Team playerTeam = new Team();
-    public Team enemyTeam = new Team();
     public List<Card> initiativeQueue = new List<Card>();
     // Start is called before the first frame update
     void Start()
@@ -42,8 +19,6 @@ public class BattleController : MonoBehaviour
     private void testBattle() {
         Debug.Log("Test battle starting...");
         Debug.Log("Assigning to player team front row left side");
-        playerTeam.assignTeamSlot(0, new Card("Test Creature 1", 10, 3, 30));
-        enemyTeam.assignTeamSlot(0, new Card("Enemy Creature", 5, 10, 20));
     }
 
     //TODO make this an onupdate thing
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+    //This gets all the children, including children of children. This populates the list depth first. 
+    public static List<GameObject> getAllChildren(GameObject obj) {
+    		List<GameObject> children = new List<GameObject>();
+    
+    		foreach(Transform child in obj.transform) {
+    			children.Add(child.gameObject);
+    			children.AddRange(getAllChildren(child.gameObject));
+    		}
+    		return children;
+    	} 
+
+    public static List<GameObject> getChildren(GameObject obj) {
+        List<GameObject> children = new List<GameObject>(); 
+        foreach(Transform child in obj.transform) {
+            children.Add(child.gameObject);
+        }
+        return children;
+    }
+
+    
+}
