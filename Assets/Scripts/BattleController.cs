e0b774b86b692a6ff430e78127747d7f52e29eac
oattions <oattions@gmail.com>
2023-09-07 22:37:35 +1000

    refactored to get rid of some redundant lines

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -11,13 +11,12 @@ public class BattleController : MonoBehaviour
     public List<Card> initiativeQueue = new List<Card>();
 
     void Awake() {
+
     }
     // Start is called before the first frame update
     void Start()
     {
-        placeCreature(Utils.ENEMY, Utils.FRONT_MID, new BaseCard("Name", 2, 2, 2));
-        placeCreature(Utils.ENEMY, Utils.BACK_RIGHT, new BaseCard("Name", 5, 2, 60));
-        placeCreature(Utils.PLAYER, Utils.BACK_MID, new BaseCard("Name", 3, 67, 2));
+        testBattle();
     }
 
     // Update is called once per frame
@@ -26,29 +25,29 @@ public class BattleController : MonoBehaviour
         
     }
 
-    private void testBattle() {
-        //generate random cards.
-    }
-
     //TODO make this an onupdate thing
     private void updateInitiativeQueue() {
         
     }
-
     public void placeCreature(int alignment, int position, BaseCard baseCard) {
         
         GameObject relevantContainer = creatureContainers[Utils.calculateRelevantContainer(alignment, position)];
         Transform containerTransform = relevantContainer.GetComponent<Transform>();
-        
         GameObject placedCreature = Instantiate(creaturePrefab, containerTransform.position, Quaternion.identity);
         Transform placedCreatureTransform =  placedCreature.GetComponent<Transform>();
+
         placedCreatureTransform.position += Vector3.back * 7; 
         placedCreatureTransform.position += Vector3.left * 3;
         placedCreatureTransform.position += ((position > 2)? Vector3.down : Vector3.up) * 48;
 
-        PlacedCreature creatureScript = placedCreature.GetComponent<PlacedCreature>();
-        creatureScript.setupCard(baseCard);
+        placedCreature.GetComponent<PlacedCreature>().setupCard(baseCard);
         placedCreature.transform.parent = playerTeam.GetComponent<Team>().teamSlots[position].transform;
-        // 5 from 3 
     } 
+
+    private void testBattle() {
+        // place the creatures first 
+        placeCreature(Utils.ENEMY, Utils.FRONT_MID, new BaseCard("Name", 2, 2, 2));
+        placeCreature(Utils.ENEMY, Utils.BACK_RIGHT, new BaseCard("Name", 5, 2, 60));
+        placeCreature(Utils.PLAYER, Utils.BACK_MID, new BaseCard("Name", 3, 67, 2));
+    }
 }
