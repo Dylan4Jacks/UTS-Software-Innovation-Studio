686108d4de8fc7a4169c0ffa348e1be876b92b85
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2023-09-28 22:44:40 +1000

    Home to Character creation

--- Assets/Scripts/Home Screen/btnStartController.cs
+++ Assets/Scripts/Home Screen/btnStartController.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using TMPro;
 
-public class StartHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class btnStartController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Color normalColor = Color.white; // The color of text normally
     public Color hoverColor = Color.red;    // The color of text on hover
@@ -12,6 +13,20 @@ public class StartHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     private void Start()
     {
         textMeshPro.color = normalColor; // Set initial color
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Check if next scene index is out of bounds, then reset to 0 (optional)
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0; // or load a specific scene, like a main menu
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
