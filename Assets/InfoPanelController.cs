ac6b6883b946df40f56c3af2d6edae2a2f8f2fb9
oattions <oattions@gmail.com>
2023-10-18 16:15:06 +1100

    info panel displays correct information

--- Assets/InfoPanelController.cs
+++ Assets/InfoPanelController.cs
@@ -10,7 +10,15 @@ public class InfoPanelController : MonoBehaviour
     public GameObject defaultView; 
     public GameObject creatureView;
     public TextMeshPro panelDescription; 
+    public TextMeshPro creatureDescription;
+    public TextMeshPro creatureName;
+    public TextMeshPro creatureAttack;
+    public TextMeshPro creatureSpeed;
+    public TextMeshPro creatureHealth;
+    public SpriteRenderer creatureSprite;
     public string defaultText = "Place your creatures!";
+    public BaseCard baseCard; 
+    public PlacedCreature placedCreature;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,14 +39,37 @@ public class InfoPanelController : MonoBehaviour
     public void viewPlacedCreature(PlacedCreature placedCreature) {
         defaultView.SetActive(false);
         creatureView.SetActive(true);
+
+        this.placedCreature = placedCreature;
+        this.baseCard = placedCreature.baseCard;
+
+        creatureName.text = placedCreature.baseCard.cardName;
+        creatureAttack.text = placedCreature.currentStrength.ToString();
+        creatureSpeed.text = placedCreature.currentSpeed.ToString();
+        creatureHealth.text = placedCreature.currentHealth.ToString();
+
+        creatureSprite.sprite = placedCreature.baseCard.sprite;
     }
 
     public void viewBaseCard(BaseCard baseCard) {
         defaultView.SetActive(false);
         creatureView.SetActive(true);
+
+        this.placedCreature = null;
+        this.baseCard = baseCard;
+
+        creatureName.text = baseCard.cardName;
+        creatureAttack.text = baseCard.strength.ToString();
+        creatureSpeed.text = baseCard.speed.ToString();
+        creatureHealth.text = baseCard.health.ToString();
+
+        creatureSprite.sprite = baseCard.sprite;
     }
 
     public void returnToDefault(string state) {
+        this.placedCreature = null;
+        this.baseCard = null;
+
         defaultView.SetActive(true);
         creatureView.SetActive(false);
         
