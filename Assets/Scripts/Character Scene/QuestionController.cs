ffbbd8de3151b992c42ec82b213eaea2d50f5009
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2023-10-22 00:07:20 +1100

    Update Fade Timings

--- Assets/Scripts/Character Scene/QuestionController.cs
+++ Assets/Scripts/Character Scene/QuestionController.cs
@@ -6,30 +6,30 @@ public class QuestionController : MonoBehaviour
 {
 
     private TMP_Text textComponent;
-    private float duration = 2f; // 2 seconds
-    private float elapsedTime = 0f;
-    private bool isFadingIn = true;
+    private float duration = 2.5f; // 2 seconds
 
     private void Start()
     {
         textComponent = GetComponent<TMP_Text>();
         // Set the text to 0% opacity at the start
         textComponent.color = new Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, 0f);
+        // Start the delayed fade-in coroutine
+        StartCoroutine(FadeInAfterDelay());
     }
-    private void Update()
+    private IEnumerator FadeInAfterDelay()
     {
-        if (isFadingIn) {
-            // Update elapsed time
+        // Wait for 1 second
+        yield return new WaitForSeconds(0.67f);
+
+        float elapsedTime = 0f;
+        while (elapsedTime < duration) {
             elapsedTime += Time.deltaTime;
 
             // Calculate alpha value
             float alpha = Mathf.Clamp01(elapsedTime / duration);
             textComponent.color = new Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, alpha);
 
-            // Check if fading is completed
-            if (alpha >= 1f) {
-                isFadingIn = false;
-            }
+            yield return null;
         }
     }
 }
